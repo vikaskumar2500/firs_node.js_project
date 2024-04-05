ae35522bbafc2bed3bfc3cda4ed567b3e51b58f7
vikaskumar2500 <vikas.nits8084@gmail.com>
2024-04-06 01:44:36 +0530

    fixed errors

--- controllers/home.js
+++ controllers/home.js
@@ -3,14 +3,22 @@ const Books = require("../modals/books");
 
 exports.homeController = async (req, res, next) => {
   try {
-    const books = await Books.findAll({include:ReturnBooks});
-    console.log(books[0]);
+    const books = await Books.findAll({ include: ReturnBooks });
+    const returnBooks = books
+      .map((book) => {
+        if (book.returnBook !== null) return book.returnBook;
+        else return null;
+      })
+      .filter((b) => b !== null);
+
+    console.log("return books", returnBooks);
     res.render("home", {
       pageTitle: "Home",
       path: "/",
       formsCSS: true,
       productCSS: true,
       books: books,
+      returnBooks,
     });
   } catch (e) {
     console.log(e);
@@ -30,10 +38,14 @@ exports.postAddBooks = async (req, res, next) => {
 
 exports.postReturnBooks = async (req, res, next) => {
   const name = req.body.name;
+  const bookId = req.body.bookId;
   const fine = req.body.fine;
+  console.log(name, fine);
   try {
-    await ReturnBooks.create({ name, fine });
+    await ReturnBooks.create({ name, fine, bookId });
+    await Books.destroy({ where: { id: bookId } });
   } catch (e) {
     console.log(e);
   }
+  res.redirect("/");
 };
--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const home = require("../controllers/home");
 
-router.post("/add-return-book", home.postReturnBooks);
+
 router.post("/add-book", home.postAddBooks);
+router.post("/add-return-books", home.postReturnBooks);
 router.get("/", home.homeController);
 
 module.exports = router;
